d4dbca18513a4644e202d426d35be629a1bb6d51
rocha19 <rochaarqueologia@outlook.com>
2022-11-18 18:08:00 -0300

    feat: add authentication user service

---
+++ src/services/auth-user-service.ts
@@ -0,0 +1,21 @@
+import 'dotenv/config'
+import { User } from '@/interfaces'
+import { prisma } from '@/shared/database'
+import { sign } from 'jsonwebtoken'
+import { compare } from 'bcrypt'
+export class AuthUserService {
+  async execute({ username, password }: User) {
+    const secret = process.env.SECRET
+    if (!secret) throw new Error('Need a valid secret')
+    const userExists = await prisma.user.findFirst({
+      where: {
+        username
+      }
+    })
+    if (!userExists) throw new Error('User incorrect')
+    const passwordMatch = await compare(password, userExists.password)
+    if (!passwordMatch) throw new Error('Password incorrect')
+    const token = sign({}, secret, { subject: userExists.id, expiresIn: '1d' })
+    return token
+  }
+}
